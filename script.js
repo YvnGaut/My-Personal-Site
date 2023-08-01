80222364128d6b0e3a51fff155c4f825a9b6581a
Yvan Gauthier <140654620+YvnGaut@users.noreply.github.com>
2023-08-01 09:02:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Function for the button on the footer 
+function Discord(){
+    window.location = "#"; 
+    window.location.href="#"; 
+}
+function Codepen() {
+    window.location = "https://codepen.io/your-work"; 
+    window.location.href= "https://codepen.io/your-work"; 
+}
+function Linkedin(){
+    window.location = "#"; 
+    window.location.href="#"; 
+}
+function Mastodon(){
+    window.location = "#"; 
+    window.location.href="#"; 
+}
+function Gitlab(){
+    window.location = "#"; 
+    window.location.href="#"; 
+}
+function Github(){
+    window.location = "https://github.com/YvnGaut" ; 
+    window.location.href = "https://github.com/YvnGaut"; 
+}
+
